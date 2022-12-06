14736b98d7bb511c7248a9e051f983932a6244ed
LeonardoMonti <LeonardoMonti>
2022-12-06 19:51:11 -0300

    feat: implements method txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    lines = []
+    try:
+        with open(path_file, mode='r') as file:
+            value = file.name.lower().split('.')[-1]
+            if value not in ['txt']:
+                print("Formato inválido", file=sys.stderr)
+
+            for line in file.readlines():
+                lines.append(line.split('\n')[0])
+
+        return lines
+
+    except:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
