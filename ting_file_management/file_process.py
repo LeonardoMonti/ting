165b652170edc4876b1ee1db37fbefa8925e73fe
LeonardoMonti <LeonardoMonti>
2022-12-08 19:33:36 -0300

    feat: implement method in file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    instance_list = []
+    for i in range(0, len(instance)):
+        instance_list.append(instance.search(i))
+
+    if path_file not in instance_list:
+        instance.enqueue(path_file)
+        file = txt_importer(path_file)
+
+        result = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len(file),
+            'linhas_do_arquivo': file,
+        }
+
+        print(f'{result}', file=sys.stdout)
 
 
 def remove(instance):
