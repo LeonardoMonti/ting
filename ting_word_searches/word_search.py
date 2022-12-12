34af60564c0e0ce03f1bf2e18ac7bca2f4555489
LeonardoMonti <LeonardoMonti>
2022-12-12 17:10:40 -0300

    feat: implement method exists word in file word search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    occurences = []
+
+    for index, line in enumerate(instance._data[0]["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            occurences.append({"linha": index + 1})
+
+    if len(occurences) > 0:
+        result.append({
+            "palavra": word,
+            "arquivo": instance._data[0]["nome_do_arquivo"],
+            "ocorrencias": occurences,
+        })
+
+    return result
 
 
 def search_by_word(word, instance):
