bf11b3d909f44910db89d3e68d33e721d7fe2d8b
LeonardoMonti <LeonardoMonti>
2022-12-12 18:51:15 -0300

    fix: method exists word in file word search 2

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,17 +1,35 @@
+from ting_file_management.file_management import txt_importer
+
+
+def output_words_found(path_file, word):
+    file = txt_importer(path_file)
+
+    if file is not None:
+        occurences = []
+        for index, line in enumerate(file):
+            if word.lower() in line.lower():
+                occurences.append({'linha': index + 1})
+
+        if len(occurences) > 0:
+            return {
+                'palavra': word,
+                'arquivo': path_file,
+                'ocorrencias': occurences,
+            }
+    return None
+
+
 def exists_word(word, instance):
     result = []
     occurences = []
 
-    for index, line in enumerate(instance._data[0]["linhas_do_arquivo"]):
-        if word.lower() in line.lower():
-            occurences.append({"linha": index + 1})
+    for index in range(0, len(instance)):
+        occurences.append(instance.search(index))
 
-    if len(occurences) > 0:
-        result.append({
-            "palavra": word,
-            "arquivo": instance._data[0]["nome_do_arquivo"],
-            "ocorrencias": occurences,
-        })
+    for path_file in occurences:
+        output_file = output_words_found(path_file, word)
+        if output_file is not None:
+            result.append(output_file)
 
     return result
 
